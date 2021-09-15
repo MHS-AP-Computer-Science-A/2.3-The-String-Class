cf74b1dc8fad90057453289f289ec127d2f13235
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-09-15 08:12:02 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,36 @@
-
+//String Class Part 1
 public class Main {
 
 	public static void main(String[] args) {
 		String str = "Avocados";
 		
-		String result = str.substring(3, 10);
-
+		//Length
+		int result = str.length();
+		System.out.println("Length of Avocados: " + result);
+		
+		//CharAt
+		char result2 = str.charAt(5);
+		System.out.println("6th char of Avocados: " + result2);
+		
+		//IndexOf
+		int result3 = str.indexOf("ca");
+		System.out.println("'ca' is located at index: " + result3);
+		
+		int result4 = str.indexOf("zebra");
+		System.out.println("'zebra' is located at index: " + result4);
+		
+		int result5 = str.indexOf("o", 3);
+		System.out.println("If we start searching at char 3, 'o' is located at index: " + result5);
+		
+		//Substring
+		String result6 = str.substring(3, 5);
+		System.out.println("substring(3, 5): " + result6);
+		
+		String result7 = str.substring(4);
+		System.out.println("substring(4): " + result7);
+		
+		String result8 = str.substring(3, 10);
+		System.out.println("substring(3, 10): " + result8);
 	}
 
 }
